cbf7ee764dc545c7b2db81bfd76d521057644f0a
ainiok <admin@ainiok.com>
2025-07-19 20:38:45 +0800

    add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-
-      </div>
-      <div className="card">
-        <button className='cursor-pointer' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <Button>Click me</Button>
       </div>
     </>
   )
